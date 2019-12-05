94af95e59d538f032716136c3cdaf466a2828128
Valentin <comasvalentin@gmail.com>
2019-12-05 18:15:38 +0100

    Started shallow_network.py

--- shallow_network.py
+++ shallow_network.py
@@ -7,13 +7,14 @@ import pickle
 import matplotlib.pyplot as plt # pour l'affichage
 import torch,torch.utils.data
 import numpy as np
+import math
 
 if __name__ == '__main__':
 
     # nombre d'image lues à chaque fois dans la base d'apprentissage (laisser à 1 sauf pour la question optionnelle sur les minibatchs)
     TRAIN_BATCH_SIZE = 1
     # on charge les données de la base MNIST
-    data = pickle.load(gzip.open('mnist.pkl.gz'),encoding='latin1')
+    data = pickle.load(gzip.open('mnist_light.pkl.gz'),encoding='latin1')
     # images de la base d'apprentissage
     train_data = torch.Tensor(data[0][0])
     # labels de la base d'apprentissage
@@ -33,23 +34,43 @@ if __name__ == '__main__':
 
     weight = 0.5
     nb_step = 0.001
-    w = np.full((785, 10), weight)
+    w_h = np.full((785, 50), weight)
+    w_o = np.full((50, 10), weight)
     for d in range(len(train_data)):
         x = train_data[d].numpy()
         x = np.append(x, 1)
-        y = np.dot(x, w)
-        dl = train_data_label[d].numpy()
-        a = nb_step * (dl - y)
-        b = np.outer(a, x)
-        w += np.transpose(b)
 
+        s_h = np.dot(x, w_h)
+        e_h = np.exp(-s_h)
+        y_h = 1 / (1 + e_h)
+
+        y_o = np.dot(y_h, w_o)
+
+
+        dl = train_data_label[d].numpy()
+        m_o = dl - y_o
+
+        a1 = np.dot(y_h, np.transpose(1 - y_h))
+        a2 = np.dot(w_o, np.transpose(m_o))
+        m_h = np.dot(a1, a2)
+        
+        dw_o = nb_step * np.outer(m_o, y_h)
+        w_o += np.transpose(dw_o)
+        dw_h = nb_step * np.outer(m_h, x)
+        w_h += np.transpose(dw_h)
+    
     nbCorrect = 0
     for d in range(len(test_data)):
-        x = test_data[d].numpy()
+        x = train_data[d].numpy()
         x = np.append(x, 1)
-        y = np.dot(x, w)
+
+        s_h = np.dot(x, w_h)
+        e_h = np.exp(-s_h)
+        y_h = 1 / (1 + e_h)
+
+        y_o = np.dot(y_h, w_o)
         index = np.argmax(test_data_label[d])
-        index2 = np.argmax(y)
+        index2 = np.argmax(y_o)
         if index == index2:
             nbCorrect += 1
     print(nbCorrect/len(test_data) * 100,"%")
